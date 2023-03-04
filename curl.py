077eeed5027435f89dd994b1d3e22280f683f39d
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-03-04 11:58:01 +0000

    Add files via upload

--- curl.py
+++ curl.py
@@ -2,8 +2,9 @@ import requests
 import sys
 url = 'https://www.microsoft.com'
 argumentos = sys.argv
+print ("\033c\033[44;30m")
 if len(argumentos)>1 :
     url=argumentos[1]
-    print (argumentos[1])
+    
 response = requests.get(url)
 print(response.content)
